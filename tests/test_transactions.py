c160cb1d24cb2ebe9bfb4ecf991260933b1e88e2
Etienne Beaulieu <ety_95@protonmail.com>
2021-01-02 22:57:12 -0500

    Add the unit test for the transaction class
    TODO:
    Add the undo tests

---
+++ tests/test_transactions.py
@@ -0,0 +1,116 @@
+import unittest
+import sys
+from tinydb import TinyDB
+from tinydb.storages import MemoryStorage
+
+sys.path.append("../src")
+from transaction import Transaction
+from cost_basis import CostBasis
+from capital_gain import CapitalGain
+
+class TestTransactions(unittest.TestCase):
+
+    def testBuy(self):
+        db = TinyDB(storage=MemoryStorage)
+
+        transaction = Transaction(db)
+        transaction.buy("2020-01-01", 1, "btc", 10000, "Description")
+
+        # Validate the cost basis
+        cost = CostBasis(db)
+        value = cost.calculate("btc", False)
+        self.assertEqual(value, 10000)
+
+        transaction.buy("2020-01-01", 0.5, "btc", 20000, "Description")
+
+        # Validate the cost basis
+        value = cost.calculate("btc", False)
+        self.assertEqual(value, 20000)
+
+        # There should be no value in the capital_gain table
+        numTaxEvents = len(db.table("capital_gain"))
+        self.assertEqual(numTaxEvents, 0)
+
+    def testBuySell(self):
+        db = TinyDB(storage=MemoryStorage)
+
+        transaction = Transaction(db)
+        self.initBuy(transaction)
+
+        cost = CostBasis(db)
+        initialCostBasis = cost.calculate("btc", False)
+
+        # Validate cost basis
+        costEth = cost.calculate("eth", False)
+        self.assertEqual(costEth, 2500)
+
+        transaction.sell("2020-01-05", 0.25, "btc", 12000, "Description")
+
+        # When you sell, the cost basis should not change
+        newCostBasis = cost.calculate("btc", False)
+        self.assertEqual(initialCostBasis, newCostBasis)
+        self.assertEqual(newCostBasis, 20000)
+        
+        # Test the capital gain
+        gain = self.returnCapitalGain(db)
+        self.assertEqual(gain, 7000)
+
+        # Test buy after sell
+        transaction.buy("2020-01-10", 0.30, "btc", 15000, "Description")
+        newCostBasis = cost.calculate("btc", False)
+        self.assertEqual(newCostBasis, 25806.45)
+
+        # Second sell test to check the capital gain with multiple tax events
+        transaction.sell("2020-01-11", 0.52, "btc", 1000, "Description")
+        newCostBasis = cost.calculate("btc", False)
+        self.assertEqual(newCostBasis, 25806.45)
+
+        # Check the capital gain with multiple tax events / sell
+        gain = self.returnCapitalGain(db)
+        self.assertEqual(gain, -5419.35)
+
+    def testExchange(self):
+        db = TinyDB(storage=MemoryStorage)
+        cost = CostBasis(db)
+
+        transaction = Transaction(db)
+        self.initBuy(transaction)
+
+        transaction.exchange("2020-01-12", 0.25, "btc", 1, "eth", 1000, "Description")
+
+        # Validate the different cost basis after an exchange of crypto
+        costBtc = cost.calculate("btc", False)
+        costEth = cost.calculate("eth", False)
+        self.assertEqual(costBtc, 20000)
+        self.assertEqual(costEth, 2000)
+        
+        gain = self.returnCapitalGain(db)
+
+        # Validate the capital gain
+        self.assertEqual(gain, -4000)
+
+
+    def testUndo(self):
+        self.assertTrue(False)
+
+
+    def initBuy(self, transaction):
+        transaction.buy("2020-01-01", 1, "btc", 10000, "Description")
+        transaction.buy("2020-01-01", 0.5, "btc", 20000, "Description")
+
+        transaction.buy("2020-01-01", 2, "eth", 5000, "Description")
+
+
+    def returnCapitalGain(self, db):
+        table = db.table("capital_gain")
+        gain = 0
+        for row in table:
+            capitalGain = CapitalGain(db, row)
+            gain += capitalGain.gain()
+
+        return gain
+
+
+if __name__ == '__main__':
+    unittest.main()
+
