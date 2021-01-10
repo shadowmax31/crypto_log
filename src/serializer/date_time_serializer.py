ce193d6b38e454850d63d20e74a8bfaeac8c2f97
Etienne Beaulieu <ety_95@protonmail.com>
2021-01-10 16:13:35 -0500

    Date format - DateTimeSerializer
    Use the config file to get the date format in date_time_serializer.py

--- src/serializer/date_time_serializer.py
+++ src/serializer/date_time_serializer.py
@@ -1,12 +1,16 @@
 from datetime import datetime
 from tinydb_serialization import Serializer
+from config import Config
 
 class DateTimeSerializer(Serializer):
     OBJ_CLASS = datetime
 
+    def __init__(self):
+        self.config = Config()
+
     def encode(self, obj):
-        return obj.strftime("%Y-%m-%dT%H:%M:%S")
+        return obj.strftime(self.config.dateFormat())
 
     def decode(self, s):
-        return datetime.strptime(s, "%Y-%m-%dT%H:%M:%S")
+        return datetime.strptime(s, self.config.dateFormat())
     
