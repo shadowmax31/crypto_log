af9d7cd3e975fbad3a1d74274301da09d20e6a2a
Etiene Beaulieu <ety_95@protonmail.com>
2021-03-14 12:48:02 -0400

    Make changes to the import of Newton's CSV
    They made a change to their CSV, it is now reflected in the system

--- src/generate_algo.py
+++ src/generate_algo.py
@@ -67,13 +67,13 @@ class GenNewton(AbstractGen):
         
         if row[1] == "TRADE":
             date = self.convertDate(row[0], "%m/%d/%Y %H:%M:%S", None)
-            amount = row[4]
-            ticker = row[5]
-            price = row[2]
+            amount = row[2]
+            ticker = row[3]
+            price = row[4]
             if price == "":
                 price = "0"
 
-            if row[3] == "CAD":
+            if row[5] == "CAD":
                 self.genTransactionString("buy", date, amount, ticker, price)
             else:
                 self.comment("This transaction type is not supported. Please add an issue on github.", row)
