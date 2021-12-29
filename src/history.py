659a1185122e67a76846ceecf5d4e8a4f426cc61
Etienne Beaulieu <ety_95@protonmail.com>
2021-12-29 10:43:38 -0500

    Export data from the DB instead of from the history

--- src/history.py
+++ src/history.py
@@ -1,6 +1,7 @@
 import sys, subprocess
 from tinydb import Query
 from const import Tables
+from util import listTickers
 
 class History:
     def __init__(self, db):
@@ -18,3 +19,25 @@ class History:
         return self.db.table(Tables.HISTORY.value).search(
                 q.command.search("buy|sell|exchange|undo")
                 )
+    
+    def from_source(self, config):
+        tables = listTickers(self.db)
+
+        for table in tables:
+            db_table = self.db.table(table)
+
+            print("# " + table)
+            for row in db_table:
+                t_type = row["type"].lower()
+                date = row["date"].strftime(config.dateFormat())
+                print("crypto " + t_type + " " + date + " " + str(row["amount"]) + " " + table + " " + str(row["price"]) + " \"" + row["description"] + "\"")
+            
+            print("")
+                
+            # "date": self.convertStrToDate(date),
+            # "amount": Decimal(str(amount)),
+            # "price": Decimal(str(price)),
+            # "description": description,
+            # "type": tType
+        
+        
