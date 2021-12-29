6fb8ab8ae77c804afdf296f8634b4aa620667afe
Etienne Beaulieu <ety_95@protonmail.com>
2021-12-28 19:03:47 -0500

    Fix list

--- src/util/helper.rs
+++ src/util/helper.rs
@@ -2,7 +2,7 @@ use rental_rod::db::Db;
 
 use super::error::CryptoError;
 
-const CAPITAL_GAIN: &str = "CAPTIAL_GAIN";
+const CAPITAL_GAIN: &str = "CAPITAL_GAIN";
 const SYSTEM_TABLES: [&str; 1]= [CAPITAL_GAIN];
 
 pub fn list_tickers(db: &Db) -> Result<Vec<String>, CryptoError>{
